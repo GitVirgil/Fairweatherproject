d3f62b956a1fb1fc132955baf48d8021b7c985cf
Virgil Pearson <pearsonvirgil@gmail.com>
2021-06-04 19:23:57 -0400

    add api key from openweatherapi

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let WAPIKEY = "39fdd2ce3e767ae30c3c0f6346afd94b";
+let wapiKey = "39fdd2ce3e767ae30c3c0f6346afd94b";
 
 let city;
 
-let queryURL = "http://api.openweathermap.org/data/2.5weather?q=" + city + "&appid=" + WAPIKEY39fdd2ce3e767ae30c3c0f6346afd94b;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + 39fdd2ce3e767ae30c3c0f6346afd94b;
 
 fetch(queryURL)
+
+
