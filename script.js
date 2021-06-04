ffcbd27c3d8fae5097d3750a434240e6ad60f770
Virgil Pearson <pearsonvirgil@gmail.com>
2021-06-04 15:53:14 -0400

    added variable and queryURL

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let WAPIKEY = "39fdd2ce3e767ae30c3c0f6346afd94b";
+
+let city;
+
+let queryURL = "http://api.openweathermap.org/data/2.5weather?q=" + city + "&appid=" + WAPIKEY39fdd2ce3e767ae30c3c0f6346afd94b;
+
+fetch(queryURL)
