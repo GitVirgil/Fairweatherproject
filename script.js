fdbf0ecb9330bb6a3a7ee8efb7f3a7ef12ec0dc7
Virgil Pearson <pearsonvirgil@gmail.com>
2021-06-04 20:05:26 -0400

    js work

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let wapiKey = "39fdd2ce3e767ae30c3c0f6346afd94b";
 
 let city;
+let state;
+let country;
 
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + 39fdd2ce3e767ae30c3c0f6346afd94b;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + wapiKey;
 
-fetch(queryURL)
+fetch(queryURL) 
 
 
+console.log(wapiKey)
